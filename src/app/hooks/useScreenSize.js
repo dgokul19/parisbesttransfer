79ba635fca289d4a56396554e5abe28f62c0d327
dgokul19 <dgokul19@gmail.com>
2024-10-10 18:32:06 +0800

    Test Fix - Window

--- src/app/hooks/useScreenSize.js
+++ src/app/hooks/useScreenSize.js
@@ -3,11 +3,11 @@
 import { useState, useEffect } from 'react';
 
 const useScreenSize = () => {
-  const [screenSize, setScreenSize] = useState({
-    width:0,
-    height: 0,
-    isMobile :  false
-  });
+    const [screenSize, setScreenSize] = useState({
+        width: typeof window !== "undefined" ? window?.innerWidth : 0,
+        height: typeof window !== "undefined" ? window?.innerHeight : 0,
+        isMobile : typeof window !== "undefined" ? ((window?.innerWidth < 650) ? true : false) : false
+    });
 
   useEffect(() => {
     const handleResize = () => {
