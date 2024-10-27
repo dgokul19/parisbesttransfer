03967d2bb9b98065ee324129c842f589e893fe89
dgokul19 <dgokul19@gmail.com>
2024-10-27 19:09:56 +0800

    AutoComplete initialization

---
+++ src/app/components/Autocomplete/index.js
@@ -0,0 +1,90 @@
+"use client";
+// CSS
+import { useState, useEffect, useRef } from "react";
+import classes from "./index.module.scss";
+
+const Autocomplete = (props) => {
+    const { options, onChange, placeholder, className } = props;
+    
+    const dropdownRef = useRef();
+
+    const [isOpen, setOpen] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+    const [selectedValue, setSelectedValue] = useState('');
+
+    useEffect(() => {
+        const handler = (event) => {
+          if (!dropdownRef.current) {
+            return;
+          }
+          if (!dropdownRef.current.contains(event.target)) {
+            setOpen(false);
+          }
+        };
+        document.addEventListener("click", handler, true);
+        return () => {
+          document.removeEventListener("click", handler);
+        };
+      }, []);
+
+    const handleSearch = (e) => {
+        const { value } = e.target;
+        setSearchValue(value);
+    }
+
+    const handleSelection = (list) => {
+        console.log({list});
+
+        setSelectedValue(list?.label);
+        setOpen(false);
+    };
+
+    const clearSelectionValue = () => {
+        setSelectedValue('');
+        setSearchValue('');
+        setOpen(true);
+    };
+
+
+    const renderDropdown = () => {
+        if (searchValue || isOpen) {
+            return (
+                <div className={classes.optionsContainer}>
+                    <ul>
+                        {options.map(list => <li key={list.label} onClick={() => handleSelection(list)}>{list.label}</li>)}
+                    </ul>
+                </div>
+            )
+        }
+    };
+
+    const renderMainContent = () => {
+        if(!selectedValue){
+            return <input placeholder={placeholder} 
+                onFocus={(e) => setOpen(true)} 
+                onChange={handleSearch} 
+                value={searchValue} />
+        }
+        return (
+            <>
+                <div className={classes.selectedContent}>
+                    <label className={classes.selectedLabel}>{selectedValue}</label>
+                    <i className="fa fa-close" onClick={clearSelectionValue}></i>
+                </div>
+            </>
+        )
+    };
+
+    console.log({isOpen, selectedValue});
+
+    return (
+        <>
+            <div className={classes.formAutoComplete} ref={dropdownRef}>
+                {renderMainContent()}
+                {renderDropdown()}
+            </div>
+        </>
+    );
+}
+
+export default Autocomplete;
--- src/app/components/Booking/BookingForm.js
+++ src/app/components/Booking/BookingForm.js
@@ -1,33 +1,23 @@
 "use client";
 import { useState } from "react";
 
+// Components
+import Autocomplete from "../Autocomplete";
 // CSS
 import classes from "../../styles/booking.module.scss";
 
 // Static Options
 import {
-    DISNEY_LAND,
     TRAVEL_LOCATION,
-    AIRPORT_LIST,
-    LOCATION_TYPE,
-    TRAIN_STATION,
-    PARIS_OPTION,
-    DEFAULT_LOCATION_OPTIONS
+    optionsList
 } from "../../constants/booking";
 import { 
     handleDateFormat,
     handleTimeFormat
 } from "../../utils/helper";
 
-const { AIRPORT, DISNEY, PARIS, FROM_AIRPORT, AIRPORT_HOTEL } = LOCATION_TYPE;
 
 const BookingForm = ({ onFormSubmit }) => {
-    
-    const [locationType, setLocationType] = useState({ ...DEFAULT_LOCATION_OPTIONS });
-    const [airportField, setAirportField] = useState({ ...DEFAULT_LOCATION_OPTIONS });
-    const [selectedTrainStation, setSelectedTrainStation] = useState({ ...DEFAULT_LOCATION_OPTIONS });
-    const [selectedParis, setSelectedParisOption] = useState({ ...DEFAULT_LOCATION_OPTIONS });
-    const [selectedDisney, setSelectedDisney] = useState({...DEFAULT_LOCATION_OPTIONS});
 
     const [ form, setForm ] = useState({
         fromLocation : '',
@@ -40,61 +30,8 @@ const BookingForm = ({ onFormSubmit }) => {
         rateCharge : '0.00',
       });
 
-      const handleAirportRadio = (e) => {
-        setAirportField({
-            ...airportField,
-            [ e.target.name] : e.target.value
-        });
-      };
-  
-      const handlePariseOption = (e) => {
-        const { name, value } = e.target;
-        setSelectedParisOption({
-            ...selectedParis,
-            [name] : value
-        });
-      };
-
-      const handleDisneySelection = (e) => {
-        const { name, value } = e.target;
-        setSelectedDisney({
-            ...selectedDisney,
-            [name] : value
-        });
-      };
-
-      const handleStationRadio = (e) => {
-        const { name, value } = e.target;
-        setSelectedTrainStation({
-            ...selectedTrainStation,
-            [name] : value
-        });
-      };
-
       const handleTopDropdown = (e) => {
         const { name, value } = e.target;
-        if(AIRPORT_LIST.includes(value)){
-            setLocationType({
-                ...locationType,
-                [name] : AIRPORT
-            });
-            setSelectedDisney({...DEFAULT_LOCATION_OPTIONS});
-            setSelectedParisOption({...DEFAULT_LOCATION_OPTIONS});
-        } else if(value === 'Disneyland Paris'){
-             setLocationType({
-                ...locationType,
-                [name] : DISNEY
-            });
-            setSelectedParisOption({...DEFAULT_LOCATION_OPTIONS});
-            setAirportField({...DEFAULT_LOCATION_OPTIONS});
-        } else {        //Paris
-            setLocationType({
-                ...locationType,
-                [name] : PARIS
-            });
-            setAirportField({...DEFAULT_LOCATION_OPTIONS});
-            setSelectedParisOption({...DEFAULT_LOCATION_OPTIONS});
-        }
         setForm({
             ...form,
             [name] : value
@@ -115,109 +52,6 @@ const BookingForm = ({ onFormSubmit }) => {
         })
       };
 
-    const renderLocationFieldOptions = (nameProperty) => {
-        if(locationType[nameProperty] === AIRPORT){
-            return (
-                // AIRPORT LOCATION
-                <div className={classes.formRow}>
-                    <ul className={classes.radioContainer}>
-                        <li>
-                            <label className="radioContainer">From Airport
-                                <input type="radio" value={FROM_AIRPORT} checked={airportField[nameProperty] === FROM_AIRPORT} name={nameProperty} onChange={handleAirportRadio} />
-                                <span className="checkmark"></span>
-                            </label>
-                        </li>
-                        <li>
-                            <label className="radioContainer">From Airport Hotels
-                                <input type="radio" value={AIRPORT_HOTEL} checked={airportField[nameProperty] === AIRPORT_HOTEL} name={nameProperty} onChange={handleAirportRadio} />
-                                <span className="checkmark"></span>
-                            </label>
-                        </li>
-                    </ul>
-                </div>
-            )
-        } else if(locationType[nameProperty] === DISNEY){
-            // DISNEY LOCATION
-            return (
-                <div className={classes.formRow}>
-                    <div className={classes.formGroup}>
-                        <select name={nameProperty} value={selectedDisney[nameProperty]} onChange={handleDisneySelection}>
-                            <option value=''> Disney pickup location</option>
-                            {renderOptions(DISNEY_LAND)}
-                        </select>
-                    </div>
-                </div>
-            )
-        } else {
-            // PARIS LOCATION
-            return (
-                <div className={classes.formRow}>
-                    <ul className={`flex-column height-auto ${classes.radioContainer}`}>
-                        {
-                            PARIS_OPTION.map(stn => (
-                                <li key={stn.label}>
-                                    <label className="radioContainer">{stn.label}
-                                        <input type="radio" value={stn.label} checked={selectedParis[nameProperty] === stn.label} name={nameProperty} onChange={handlePariseOption} />
-                                        <span className="checkmark"></span>
-                                    </label>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            )
-        }
-    }
-
-    const renderPickUpLocationDetails = (nameProperty) => {
-        if(locationType[nameProperty] === AIRPORT && airportField[nameProperty] === AIRPORT_HOTEL){
-            return (
-                <div className={classes.formRow}>
-                    <div className={classes.formGroup}>
-                        <input name={nameProperty} placeholder={`Location Details`}
-                            value={form.locationDetails}
-                            onChange={handleChange} />
-                    </div>
-                </div>
-            )
-        } else if (selectedParis[nameProperty] && selectedParis[nameProperty] !== 'From Paris Train Stations'){
-            return (
-                <div className={classes.formRow}>
-                    <div className={classes.formGroup}>
-                        <input name={nameProperty} placeholder={`Location Details`}
-                            value={form.locationDetails}
-                            onChange={handleChange} />
-                    </div>
-                </div>
-            )
-        }
-        return null;
-    };
-
-    const renderParisTrainStation = (nameProperty) => {
-        if(selectedParis[nameProperty] === 'From Paris Train Stations'){
-            return (
-                <div className={`flex-column ${classes.formRow}`}>
-                    
-                    <h4 className="text-left">Select Paris Train Station: </h4>
-                    <ul className={`flex-column height-auto ${classes.radioContainer}`}>
-                        {
-                            TRAIN_STATION.map(stn => (
-                                <li key={stn.value}>
-                                    <label className="radioContainer">{stn.label}
-                                        <input type="radio" value={stn.value} checked={selectedTrainStation === stn.value} name={nameProperty} onChange={handleStationRadio} />
-                                        <span className="checkmark"></span>
-                                    </label>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            );
-        }
-        return null;
-    }
-
     return (
         <div className={classes.bookingWrapper}>
             <form className={classes.bookingForm}>
@@ -230,17 +64,13 @@ const BookingForm = ({ onFormSubmit }) => {
                     <div className={classes.contentColumn}>
                         <div className={classes.formRow}>
                             <div className={classes.formGroup}>
-                                <select name={`fromLocation`} value={form.fromLocation} onChange={handleTopDropdown}>
-                                    <option value=''> Please select the start destination </option>
-                                    {renderOptions(TRAVEL_LOCATION)}
-                                </select>
-                                <i className={`fa fa-globe`}></i>
+                                <Autocomplete
+                                    placeholder={`Enter the start destination`}
+                                    className={classes.autoComplete}
+                                    options={optionsList}>
+                                </Autocomplete>
                             </div>
                         </div>
-
-                        {form.fromLocation && renderLocationFieldOptions(`fromLocation`)}
-                        {renderPickUpLocationDetails(`fromLocation`)}
-                        {renderParisTrainStation(`fromLocation`)}
 
                         <div className={`flex-column ${classes.formRow}`}>
                             <label className={classes.mobileLabel}>Pickup Date</label>
@@ -264,18 +94,13 @@ const BookingForm = ({ onFormSubmit }) => {
                     <div className={classes.contentColumn}>
                         <div className={classes.formRow}>
                             <div className={classes.formGroup}>
-                                <select name={`toLocation`}  value={form.toLocation} onChange={handleTopDropdown}>
-                                    <option value=''> Please select the end destination </option>
-                                    {renderOptions(TRAVEL_LOCATION)}
-                                </select>
-                                <i className={`fa fa-globe`}></i>
+                                <Autocomplete
+                                    placeholder={`Enter the to destination`}
+                                    className={classes.autoComplete}
+                                    options={optionsList}>
+                                </Autocomplete>
                             </div>
                         </div>
-
-                        
-                        {form.toLocation && renderLocationFieldOptions(`toLocation`)}
-                        {renderPickUpLocationDetails(`toLocation`)}
-                        {renderParisTrainStation(`toLocation`)}
 
                         <div className={`flex-column ${classes.formRow}`}>
                             <label className={classes.mobileLabel}>Pickup time</label>
